1c6a8f03d1fe93397b170a83a719563398c1ddda
kazuma1989 <funifuni.1204@gmail.com>
2019-10-18 22:20:14 +0900

    refactor: Use gin.Default()

--- main.go
+++ main.go
@@ -11,13 +11,11 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
+	router := gin.Default()
 	router.LoadHTMLGlob("*.html")
 
 	router.GET("/", func(c *gin.Context) {
