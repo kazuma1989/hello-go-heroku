75150423e6e3258046b5816592000ec7bd6a6807
kazuma1989 <funifuni.1204@gmail.com>
2019-10-18 21:56:54 +0900

    fix: Run server locally

--- main.go
+++ main.go
@@ -18,11 +18,10 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	router.LoadHTMLGlob("*.html")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	router.Run(":" + port)
