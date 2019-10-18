b0733b30f8bb418a2c8e4e3d5559bf02faa741aa
kazuma1989 <funifuni.1204@gmail.com>
2019-10-19 01:17:40 +0900

    refactor: Use goquery directly
    Colly's callback style was hard to understand...

--- go.mod
+++ go.mod
@@ -3,15 +3,8 @@ module github.com/kazuma1989/hello-go-heroku
 go 1.13
 
 require (
-	github.com/PuerkitoBio/goquery v1.5.0 // indirect
-	github.com/antchfx/htmlquery v1.1.0 // indirect
-	github.com/antchfx/xmlquery v1.1.0 // indirect
-	github.com/antchfx/xpath v1.1.0 // indirect
+	github.com/PuerkitoBio/goquery v1.5.0
 	github.com/gin-gonic/gin v1.4.0
-	github.com/gocolly/colly v1.2.0
 	github.com/heroku/x v0.0.10
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/text v0.3.0
 )
--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
-	"github.com/gocolly/colly"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
@@ -35,13 +35,17 @@ func main() {
 	})
 
 	router.GET("/nagayo.ics", func(ctx *gin.Context) {
-		collector := colly.NewCollector()
+		doc, err := goquery.NewDocument("http://nagayo.sakura.ne.jp/cgi/schedule/schedule.cgi?year=2019&month=10")
+		if err != nil {
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
 
 		var events []string
 		// nth-of-type(3): "3" represents Tuesday
-		collector.OnHTML("tr >td:nth-of-type(3)", func(elem *colly.HTMLElement) {
+		doc.Find("tr >td:nth-of-type(3)").Each(func(i int, elem *goquery.Selection) {
 			decoded, _ := ioutil.ReadAll(transform.NewReader(
-				strings.NewReader(elem.Text),
+				strings.NewReader(elem.Text()),
 				japanese.EUCJP.NewDecoder(),
 			))
 			innerText := string(decoded)
@@ -49,30 +53,28 @@ func main() {
 			// 12有楽町山野 -> [12有楽町山野, 12, 有楽町山野]
 			r := regexp.MustCompile(`^([0-9]{1,2})(.*$)`)
 			match := r.FindStringSubmatch(innerText)
-
-			if len(match) == 3 {
-				date := match[1]
-				summary := match[2]
-
-				if summary != "" {
-					event := "BEGIN:VEVENT\n"
-					event += "SUMMARY:" + summary + "\n"
-					event += "LOCATION:ヤマノミュージックサロン有楽町 〒100-0006\\, 東京都千代田区\\, 有楽町2丁目10番1号\n"
-					event += "DTSTART;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT193000", date) + "\n"
-					event += "DTEND;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT203000", date) + "\n"
-					event += "END:VEVENT\n"
-
-					events = append(events, event)
-				}
+			if len(match) != 3 {
+				return
 			}
+
+			date := match[1]
+			summary := match[2]
+			if summary == "" {
+				return
+			}
+
+			event := "BEGIN:VEVENT\n"
+			event += "SUMMARY:" + summary + "\n"
+			event += "LOCATION:ヤマノミュージックサロン有楽町 〒100-0006\\, 東京都千代田区\\, 有楽町2丁目10番1号\n"
+			event += "DTSTART;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT193000", date) + "\n"
+			event += "DTEND;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT203000", date) + "\n"
+			event += "END:VEVENT\n"
+
+			events = append(events, event)
 		})
 
-		collector.OnScraped(func(r *colly.Response) {
-			ctx.Header("Content-Type", "text/calendar")
-			ctx.String(http.StatusOK, "BEGIN:VCALENDAR\n"+strings.Join(events, "")+"END:VCALENDAR\n")
-		})
-
-		collector.Visit("http://nagayo.sakura.ne.jp/cgi/schedule/schedule.cgi?year=2019&month=11")
+		ctx.Header("Content-Type", "text/calendar")
+		ctx.String(http.StatusOK, "BEGIN:VCALENDAR\n"+strings.Join(events, "")+"END:VCALENDAR\n")
 	})
 
 	router.Run(":" + port)
