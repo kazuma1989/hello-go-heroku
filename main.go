ee4d432b47e51ccc44ef63a5ff4eb8aeadba46fc
kazuma1989 <funifuni.1204@gmail.com>
2019-10-19 09:26:55 +0900

    refactor: Extract parsing logic

--- main.go
+++ main.go
@@ -35,47 +35,52 @@ func main() {
 	})
 
 	router.GET("/nagayo.ics", func(ctx *gin.Context) {
-		doc, err := goquery.NewDocument("http://nagayo.sakura.ne.jp/cgi/schedule/schedule.cgi?year=2019&month=10")
+		doc, err := goquery.NewDocument("http://nagayo.sakura.ne.jp/cgi/schedule/schedule.cgi?year=2019&month=11")
 		if err != nil {
 			ctx.Status(http.StatusInternalServerError)
 			return
 		}
 
-		var events []string
-		// nth-of-type(3): "3" represents Tuesday
-		doc.Find("tr >td:nth-of-type(3)").Each(func(i int, elem *goquery.Selection) {
-			decoded, _ := ioutil.ReadAll(transform.NewReader(
-				strings.NewReader(elem.Text()),
-				japanese.EUCJP.NewDecoder(),
-			))
-			innerText := string(decoded)
-
-			// 12有楽町山野 -> [12有楽町山野, 12, 有楽町山野]
-			r := regexp.MustCompile(`^([0-9]{1,2})(.*$)`)
-			match := r.FindStringSubmatch(innerText)
-			if len(match) != 3 {
-				return
-			}
-
-			date := match[1]
-			summary := match[2]
-			if summary == "" {
-				return
-			}
-
-			event := "BEGIN:VEVENT\n"
-			event += "SUMMARY:" + summary + "\n"
-			event += "LOCATION:ヤマノミュージックサロン有楽町 〒100-0006\\, 東京都千代田区\\, 有楽町2丁目10番1号\n"
-			event += "DTSTART;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT193000", date) + "\n"
-			event += "DTEND;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT203000", date) + "\n"
-			event += "END:VEVENT\n"
-
-			events = append(events, event)
-		})
+		events := parse(doc)
 
 		ctx.Header("Content-Type", "text/calendar")
 		ctx.String(http.StatusOK, "BEGIN:VCALENDAR\n"+strings.Join(events, "")+"END:VCALENDAR\n")
 	})
 
 	router.Run(":" + port)
+}
+
+func parse(doc *goquery.Document) (events []string) {
+	// nth-of-type(3): "3" represents Tuesday
+	doc.Find("tr >td:nth-of-type(3)").Each(func(i int, elem *goquery.Selection) {
+		decoded, _ := ioutil.ReadAll(transform.NewReader(
+			strings.NewReader(elem.Text()),
+			japanese.EUCJP.NewDecoder(),
+		))
+		innerText := string(decoded)
+
+		// 12有楽町山野 -> [12有楽町山野, 12, 有楽町山野]
+		r := regexp.MustCompile(`^([0-9]{1,2})(.*$)`)
+		match := r.FindStringSubmatch(innerText)
+		if len(match) != 3 {
+			return
+		}
+
+		date := match[1]
+		summary := match[2]
+		if summary == "" {
+			return
+		}
+
+		event := "BEGIN:VEVENT\n"
+		event += "SUMMARY:" + summary + "\n"
+		event += "LOCATION:ヤマノミュージックサロン有楽町 〒100-0006\\, 東京都千代田区\\, 有楽町2丁目10番1号\n"
+		event += "DTSTART;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT193000", date) + "\n"
+		event += "DTEND;TZID=Asia/Tokyo:" + fmt.Sprintf("201911%02sT203000", date) + "\n"
+		event += "END:VEVENT\n"
+
+		events = append(events, event)
+	})
+
+	return events
 }
